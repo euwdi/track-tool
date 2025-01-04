126db0c19e4154985573e016f874ce942b7dd3ef
eugen1337 <evgenijkurakin478@gmail.com>
2025-01-05 00:39:55 +0700

    add svg support

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
 
 import path from "node:path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       "@assets": path.resolve(__dirname, "./src/assets"),
       "@common": path.resolve(__dirname, "./src/common"),
       "@features": path.resolve(__dirname, "./src/features"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
     },
   },
   css: {

06e22cb928e617f173c016309aee6bd7f76f8447
eugen1337 <evgenijkurakin478@gmail.com>
2025-05-24 16:50:14 +0700

    add mobile menu

--- src/common/layout/Layout.tsx
+++ src/common/layout/Layout.tsx
@@ -1,26 +1,51 @@
-import { FC } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import classes from "./style.module.scss";
-import { Outlet, useNavigate } from "react-router";
+import { Outlet } from "react-router";
 import Logo from "@/assets/logo.svg?react";
 import BurgerMenu from "@/assets/burgerMenu.svg?react";
-import ProfileIcon from "@/assets/profile.svg?react";
-import { Routes } from "@/router/router";
-import { useUserStore } from "@/stores/userStore";
 import { Menu } from "@/common/components/Menu/Menu";
 import UserProfileDropdown from "@/features/users/userProfileDropdown/UserProfileDropdown";
 
 const Layout: FC<React.InputHTMLAttributes<HTMLInputElement>> = () => {
-  const navigator = useNavigate();
-  const { logout } = useUserStore();
+  const [openedMenu, setOpenedMenu] = useState<boolean>(false);
+
+  const moobileMenuRef = useRef<HTMLDivElement>(null);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      moobileMenuRef.current &&
+      !moobileMenuRef.current.contains(event.target as Node)
+    ) {
+      setOpenedMenu(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <>
       <header className={classes.header}>
-        <BurgerMenu className={classes.burger} />
+        <BurgerMenu
+          className={classes.burger}
+          onClick={() => setOpenedMenu(!openedMenu)}
+        />
         <Logo className={classes.logo} />
 
         <div className={classes.actionsContainer}>
-          <Menu />
+          <div className={classes.baseMenu}>
+            <Menu />
+          </div>
+          {openedMenu && (
+            <div className={classes.mobileMenu} ref={moobileMenuRef}>
+              <Menu />
+            </div>
+          )}
+
           {/* <ProfileIcon className={classes.actionIcon} /> */}
           <UserProfileDropdown />
           {/* <button
